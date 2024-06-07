b453fdda679863acea04f544eac7431ff7856b26
g-xxoxx <trungduc05112000@gmail.com>
2024-06-07 13:56:45 +0700

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,115 @@
+import { Email, Facebook, Instagram, LocationCity, Phone, Pinterest, Twitter } from '@material-ui/icons'
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  margin-top: 10px;
+`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`
+const Logo = styled.h1``
+
+const Desc = styled.p`
+  margin: 20px 0;
+`
+const SocialContainer = styled.div`
+  display: flex;
+`
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${props => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+`
+
+const Center = styled.div`
+  flex: 1;
+  margin-left: 5px;
+  padding: 20px;
+`
+const Title = styled.h4`
+  margin-bottom: 30px;
+`
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`
+const Payment = styled.img``
+
+const Footer = () => {
+    return (
+        <Container>
+            <Left>
+                <Logo>511Store.</Logo>
+                <Desc>There are many variations of passages of Lorem Ipsum availble, the majoriry have suffered
+                    alteration in some form, by injected humour, or randomised words which don't look even slight believable
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon color='3B5999'>
+                        <Facebook />
+                    </SocialIcon>
+                    <SocialIcon color='E4405F'>
+                        <Instagram />
+                    </SocialIcon>
+                    <SocialIcon color='55ACEE'>
+                        <Twitter />
+                    </SocialIcon>
+                    <SocialIcon color='E60023'>
+                        <Pinterest />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Terms</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem><LocationCity /> Da Nang, Viet Nam</ContactItem>
+                <ContactItem><Phone /> +84 917643674</ContactItem>
+                <ContactItem><Email />trungduc051100@gmail.com</ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
+        </Container>
+    )
+}
+
+export default Footer
